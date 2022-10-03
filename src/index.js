f4649f71b6128d2d88b05cbb34caf13d9b4d894f
Ian <iancabello16@gmail.com>
2022-10-03 19:06:57 -0400

    Index to /root

--- src/index.js
+++ src/index.js
@@ -2,32 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import NavBar from './Nav';
-import Intro from './/components/Intro';
-import Work from './Work';
-import App from './App';
+import App from './components/App';
+import Navbar from './components/Nav/Nav';
+import Home from './components/Home';
 
 import reportWebVitals from './reportWebVitals';
 
-const navigation = ReactDOM.createRoot(document.getElementById('nav-menu'))
+// const navigation = ReactDOM.createRoot(document.getElementById('nav-container'))
 const content = ReactDOM.createRoot(document.getElementById('content'));
 
-navigation.render(
-  <React.StrictMode>
-    <NavBar />
-  </React.StrictMode>
-)
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+// navigation.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
 
 content.render(
   <React.StrictMode>
-
-    <Intro />
-    <Work />
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
