b04a7981cb2ac0fea9271fbaac14fe718d86a3c4
Ian <iancabello16@gmail.com>
2022-10-10 16:04:15 -0400

    Updated links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,13 +3,13 @@ function About () {
         <div className='content_container'>
             <main className="sidebyside_content">
                 <div className="img_container">
-                    <img src="pending_artwork.jpg" alt="main_page_img" id="img_content"/>
+                    <img src="My_Animals.png" alt="My_Animals" id="img_content"/>
                 </div>
                 <div className="text_content">
                     <h1 className='page_title'>About Me</h1>
                     <p className='description'>   
                     In 2017, I started my professional career performing internal, IT audits where I used my coding knowledge
-                    to enhance control testing through code inspection. Moving on further into audit, I began to miss the excitement 
+                    to enhance control testing through code inspection. Moving on further into external audit, I began to miss the excitement 
                     and thrills of creating things through coding. In 2021, I restarted my journey of becoming a developer.
                     </p>
                     <p className='description'>
@@ -26,10 +26,14 @@ function About () {
             <footer class="footer_container">
                 <div class="footer_alignment">
                     <h2 className="footer_header"> Want to get in touch? </h2>
-                    <p className="footer_content"> Please send me an email at &nbsp;
-                        <a className='footer_link' href="mailto:me@ollebac.dev">me@ollebac.dev</a>.</p>
+                    <p className="footer_content"> Please send me an email at&nbsp;
+                        <a className='footer_link' href="mailto:me@ollebac.dev">me@ollebac.dev</a>
+                        .
+                    </p>
                     <p className="footer_content"> 
-                        Artwork on this page is done by the lovely *Pending Artist Name*, for my personal use only.
+                        Artwork on this page is done by the lovely&nbsp;
+                            <a className="footer_link" href="www.fiverr.com/l0a0n0">Lan (aka: l0a0n0)</a>
+                        , for my personal use only.
                     </p>
                     <p className="footer_content"> Website is designed by me and built in React. </p>
                 </div>
