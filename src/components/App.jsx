f0ef150e2bd0b9bbcb21063bae4f3037073da236
Ian <iancabello16@gmail.com>
2022-10-27 17:12:18 -0400

    Added potionomics page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,13 @@ import './Footer.css';
 import './Home.css';
 import './About.css';
 import './Projects.css';
-import './Error.css';
+import './PotionIngredients.css';
 
 import Navbar from './Nav';
 import Home from './Home';
 import About from './About';
 import Projects from './Projects';
+import PotionIngredients from './PotionIngredients';
 import Error from './Error';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -17,19 +18,27 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (    
       <Router>
-        <div className="webpage_container">
-          <Navbar />
+        <div className="webpage_container">          
           <Switch>
             <Route exact path="/"> 
+              <Navbar />
               <Home />
             </Route>
             <Route exact path="/about">
+              <Navbar />
               <About />
             </Route>
             <Route exact path="/projects">
+              <Navbar />
               <Projects />
             </Route>
+            <Route exact path="/potionomics">
+              <div className='potion_page'>
+                <PotionIngredients />
+              </div>
+            </Route>
             <Route path="/*">
+              <Navbar />
               <Error />
             </Route>
           </Switch>
